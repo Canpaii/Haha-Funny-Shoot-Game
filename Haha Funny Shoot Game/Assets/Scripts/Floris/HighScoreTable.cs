47a5eef5be3699024720a508a4df0d5c5aba66d5
FlorisBolt <floris@bolt.tk>
2024-04-02 15:36:21 +0200

    Re-added tileable assets (now working)
    yippie

--- Haha Funny Shoot Game/Assets/Scripts/Floris/HighScoreTable.cs
+++ Haha Funny Shoot Game/Assets/Scripts/Floris/HighScoreTable.cs
@@ -8,6 +8,8 @@ public class HighScoreTable : MonoBehaviour
 {
     private Transform entryContainer;
     private Transform entryTemplate;
+    private List<HighscoreEntry> highscoreEntryList;
+    private List<Transform> highscoreEntryTransformList;
     private void Awake()
     {
         entryContainer = transform.Find("HighScoreEntryContainer");
@@ -15,33 +17,90 @@ public class HighScoreTable : MonoBehaviour
 
         entryTemplate.gameObject.SetActive(false);
 
-        float templateHeight = 74f;
-        for (int i = 0; i < 10; i++)
+        highscoreEntryList = new List<HighscoreEntry>() 
         {
-            Transform entryTransform = Instantiate(entryTemplate, entryContainer);
-            RectTransform entryRectTransform = entryTransform.GetComponent<RectTransform>();
-            entryRectTransform.anchoredPosition = new Vector2(0, -templateHeight * i);
-            entryTransform.gameObject.SetActive(true);
+            new HighscoreEntry{ score = 521854, name = "AAA" },
+            new HighscoreEntry{ score = 358462, name = "ANN" },
+            new HighscoreEntry{ score = 785123, name = "CAT" },
+            new HighscoreEntry{ score = 15524, name = "JON" },
+            new HighscoreEntry{ score = 897621, name = "JOE" },
+            new HighscoreEntry{ score = 68245, name = "MIK" },
+            new HighscoreEntry{ score = 872931, name = "DAV" },
+            new HighscoreEntry{ score = 542024, name = "MAX" },
+        };
 
-            int rank = i + 1;
-            string rankString;
-            switch (rank)
+        string jsonString = PlayerPrefs.GetString("highscoreTable");
+        Highscores highscores = JsonUtility.FromJson<Highscores>(jsonString);
+
+        for (int i = 0; i < highscoreEntryList.Count; i++)
+        {
+            for (int j = i + 1; j < highscoreEntryList.Count; j++)
             {
-                default:rankString = rank + "TH"; break;
-
-                case 1: rankString = "1ST"; break;
-                case 2: rankString = "2ND"; break;
-                case 3: rankString = "3RD"; break;
+                if (highscoreEntryList[j].score > highscoreEntryList[i].score)
+                {
+                    HighscoreEntry tmp = highscoreEntryList[i];
+                    highscoreEntryList[i] = highscoreEntryList[j];
+                    highscoreEntryList[j] = tmp;
+                }
             }
-
-            entryTransform.Find("posText").GetComponent<TextMeshProUGUI>().text = rankString;
-
-            int score = Random.Range(0, 10000);
-
-            entryTransform.Find("scoreText").GetComponent<TextMeshProUGUI>().text = score.ToString();
-
-            string name = "AAA";
-            entryTransform.Find("nameText").GetComponent<TextMeshProUGUI>().text = name;
         }
+        highscoreEntryTransformList = new List<Transform>();
+        foreach (HighscoreEntry highscoreEntry in highscoreEntryList)
+        {
+            CreateHighscoreEntryTransform(highscoreEntry, entryContainer, highscoreEntryTransformList);
+        }
+ 
+        Highscores highscores1 = new Highscores { highscoreEntryList = highscoreEntryList };
+        string json = JsonUtility.ToJson(highscores);
+        PlayerPrefs.SetString("highscoreTable", json);
+        PlayerPrefs.Save();
+        Debug.Log(PlayerPrefs.GetString("highscoreTable")); 
+        
+    }
+
+    private void CreateHighscoreEntryTransform(HighscoreEntry highscoreEntry, Transform container, List<Transform> transformList)
+    {
+        float templateHeight = 74f;
+        Transform entryTransform = Instantiate(entryTemplate, container);
+        RectTransform entryRectTransform = entryTransform.GetComponent<RectTransform>();
+        entryRectTransform.anchoredPosition = new Vector2(0, -templateHeight * transformList.Count);
+        entryTransform.gameObject.SetActive(true);
+
+        int rank = transformList.Count + 1;
+        string rankString;
+        switch (rank)
+        {
+            default: rankString = rank + "TH"; break;
+
+            case 1: rankString = "1ST"; break;
+            case 2: rankString = "2ND"; break;
+            case 3: rankString = "3RD"; break;
+        }
+
+        entryTransform.Find("posText").GetComponent<TextMeshProUGUI>().text = rankString;
+
+        int score = highscoreEntry.score;
+
+        entryTransform.Find("scoreText").GetComponent<TextMeshProUGUI>().text = score.ToString();
+
+        string name = highscoreEntry.name;
+
+        entryTransform.Find("nameText").GetComponent<TextMeshProUGUI>().text = name;
+
+        transformList.Add(entryTransform);
+    }
+    
+    private class Highscores
+    {
+        public List<HighscoreEntry> highscoreEntryList;
+    }
+    
+    [System.Serializable]
+
+    private class HighscoreEntry
+    {
+        public int score;
+        public string name;
+
     }
 }
